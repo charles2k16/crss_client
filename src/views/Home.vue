a0314caf4444cae3c22ad4b47bff45923f61880a
Charles Obeng <charlesobeng000@gmail.com>
2020-10-14 23:29:57 +0000

    set up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h4>FOMR FOR REGISTRATION</h4>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  name: "Home"
 };
 </script>
